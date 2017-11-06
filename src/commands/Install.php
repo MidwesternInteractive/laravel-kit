e88447183627ce1b3fe85ad997591118b081cbd3
Ryan Doss <ryan.doss@gmail.com>
2017-11-06 13:49:34 -0600

    finished basic install

--- src/commands/Install.php
+++ src/commands/Install.php
@@ -4,10 +4,9 @@ namespace MWI\LaravelKit\Commands;
 
 use App\User;
 use Illuminate\Console\Command;
-use Illuminate\Foundation\Console\Artisan;
 use Illuminate\Support\Facades\Hash;
-use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class Install extends Command
 {
@@ -33,25 +32,28 @@ class Install extends Command
     {
         file_put_contents(base_path('.gitignore'), "/public/css\n/public/js\n", FILE_APPEND);
 
-        Artisan::call('make:auth');
+        $this->runArtisanCalls();
 
-        // Publish Service Providers
-        Artisan::call('vendor:publish --provider="MWI\LaravelKit\LaravelKitServiceProvider"');
-        Artisan::call('vendor:publish --provider="Spatie\Permission\PermissionServiceProvider"');
-
-        // Create base Auth and Migrate
-        Artisan::call('migrate');
-
+        // Create Super User
         $user = User::create([
             'name'      => 'mwi',
             'email'     => 'support@buildmidwestern.com',
             'password'  => Hash::make('123mwi'),
         ]);
 
-        // Create Base Role and Assign to user.
-        Role::create(['name' => 'super']);
-        $user->assignRole('super');
-
         $this->comment('MWI Laravel Kit Installed.');
+    }
+
+    public function runArtisanCalls()
+    {
+        // Create Auth structure
+        $this->call('make:auth');
+
+        // Publish Service Providers
+        $this->call('vendor:publish', ['--provider' => 'MWI\LaravelKit\LaravelKitServiceProvider']);
+        $this->call('vendor:publish', ['--provider' => 'Spatie\Permission\PermissionServiceProvider']);
+
+        // Create base Auth and Migrate
+        $this->call('migrate');
     }
 }
