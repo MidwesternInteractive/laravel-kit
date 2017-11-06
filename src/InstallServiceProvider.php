d7beee38b3c8c7db6b850236c0bd53775ea8294c
Ryan Doss <ryan.doss@gmail.com>
2017-11-06 09:36:32 -0600

    added tag to publish

--- src/InstallServiceProvider.php
+++ src/InstallServiceProvider.php
@@ -10,7 +10,7 @@ class InstallServiceProvider extends ServiceProvider
     {
         $this->publishes([
             __DIR__.'/app/' => app_path(),
-        ]);
+        ], 'app');
     }
 
     public function register()
