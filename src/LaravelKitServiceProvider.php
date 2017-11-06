48c79aeb704e439ef7a08c255100f49370214fc1
Ryan Doss <ryan.doss@gmail.com>
2017-11-06 11:26:28 -0600

    updated composer.json to auto include service provider

---
+++ src/LaravelKitServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace MWI\LaravelKit;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelKitServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/app/' => app_path(),
+        ], 'app');
+
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                Commands\Install::class,
+            ]);
+        }
+    }
+
+    public function register()
+    {
+        //
+    }
+
+    public function provides()
+    {
+        //
+    }
+}
